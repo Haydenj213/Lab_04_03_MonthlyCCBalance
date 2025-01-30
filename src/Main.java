3ee0b70e2b9d56d4ea345b20b6f0aa8eeec287c3
Haydenj213 <johns3hm@mail.uc.edu>
2025-01-30 16:29:52 -0500

    Added main code.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+
+    {
+        double cardBalance = 5000.00;
+        double interestRate = 0.17;
+        double monthlyInterestRate = 0.0;
+        double cardBalanceOneMonth = 0.0;
+        double cardBalanceTwoMonth = 0.0;
+
+        monthlyInterestRate = 30 * (interestRate/365);
+        cardBalanceOneMonth = (monthlyInterestRate * cardBalance);
+        cardBalanceTwoMonth = monthlyInterestRate * (cardBalance + cardBalanceOneMonth);
+
+
+
+        System.out.println("Your interest due after one month will be $" + cardBalanceOneMonth + ". Your interest due after two months will be $" + cardBalanceTwoMonth);
+
     }
 }
